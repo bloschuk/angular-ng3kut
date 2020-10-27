035920a8a8b6ee110388c7aacca95ce87a488496
bloschuk <72076265+bloschuk@users.noreply.github.com>
2020-10-27 21:34:59 +0300

    Create work-2.component.ts

---
+++ work-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'work-2.html',
+  styleUrls: [ 'work-2.css' ]
+})
+export class AppComponent  {}
+
+
